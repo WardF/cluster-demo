17500de95ae6c6766a3601a0be8d7860af19bfaa
Ward Fisher <wfisher@ucar.edu>
2016-02-10 15:17:04 -0700

    Throwing xterm windows to background.

--- custom_up.sh
+++ custom_up.sh
@@ -6,34 +6,33 @@
 
 vagrant up master
 
-
-xterm -bg black -fg white -e "vagrant up node1 && echo 'Press [Return] to close' && read"
+xterm -bg black -fg white -e "vagrant up node1 && echo 'Press [Return] to close' && read" &
 sleep 10
 
 
-xterm -bg black -fg white -e "vagrant up node2 && echo 'Press [Return] to close' && read"
+xterm -bg black -fg white -e "vagrant up node2 && echo 'Press [Return] to close' && read" &
 sleep 10
 
 
-xterm -bg black -fg white -e "vagrant up node3 && echo 'Press [Return] to close' && read"
+xterm -bg black -fg white -e "vagrant up node3 && echo 'Press [Return] to close' && read" &
 sleep 10
 
 
-xterm -bg black -fg white -e "vagrant up node4 && echo 'Press [Return] to close' && read"
+xterm -bg black -fg white -e "vagrant up node4 && echo 'Press [Return] to close' && read" &
 sleep 10
 
 
-xterm -bg black -fg white -e "vagrant up node5 && echo 'Press [Return] to close' && read"
+xterm -bg black -fg white -e "vagrant up node5 && echo 'Press [Return] to close' && read" &
 sleep 10
 
 
-xterm -bg black -fg white -e "vagrant up node6 && echo 'Press [Return] to close' && read"
+xterm -bg black -fg white -e "vagrant up node6 && echo 'Press [Return] to close' && read" &
 sleep 10
 
 
-xterm -bg black -fg white -e "vagrant up node7 && echo 'Press [Return] to close' && read"
+xterm -bg black -fg white -e "vagrant up node7 && echo 'Press [Return] to close' && read" &
 sleep 10
 
 
-xterm -bg black -fg white -e "vagrant up node8 && echo 'Press [Return] to close' && read"
+xterm -bg black -fg white -e "vagrant up node8 && echo 'Press [Return] to close' && read" &
 sleep 10
